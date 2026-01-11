4b3bdbb5791b6dc7f20a45dce62144c71c65c15a
joel joseph <joeljwebdev@gmail.com>
2026-01-11 12:51:19 -0800

    feat: added rabbitmq connection logic and graceful shutdown to server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(rabbitConnString)
+	if err != nil {
+		log.Fatalf("could to connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	fmt.Println("RabbitMQ connection successfull!")
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	done := make(chan bool, 1)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	<-done
+	fmt.Println("Server shutting down...")
+
 }
